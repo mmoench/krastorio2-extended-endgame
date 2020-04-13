19a2cd5bc0ed1f7f7426d49115e9bdc8245ba4f3
Michael Mönch <michael.moench@offerista.com>
2020-04-13 13:44:43 +0200

    improved tech-unlock

--- prototypes/intergalactic-transceiver-updates.lua
+++ prototypes/intergalactic-transceiver-updates.lua
@@ -1,33 +1,3 @@
--- Replace the standard tech to unlock our own transceiver instead of the default one:
-data:extend(
-{
-	{
-		type = "technology",
-		name = "kr-intergalactic-transceiver",
-		mod = "Krastorio2",
-		icon = "__Krastorio2__/graphics/technologies/intergalactic-transceiver.png",
-		icon_size = 128,
-		effects =
-		{
-			{
-				type = "unlock-recipe",			
-				recipe = "kee-intergalactic-transceiver-loading"
-			}
-		},
-		prerequisites = {"kr-imersium-processing", "kr-energy-control-unit", "kr-singularity-tech-card"},
-		unit =
-		{
-			count = 3000,
-			ingredients = 
-			{
-				{"production-science-pack", 1},
-				{"utility-science-pack", 1},
-				{"space-science-pack", 1},
-				{"matter-tech-card", 1},
-				{"advanced-tech-card", 1},
-				{"singularity-tech-card", 1}
-			},
-			time = 60
-		}
-    }
-})
+-- Modify the transceiver tech to unlock our building instead of the original:
+krastorio.technologies.addUnlockRecipe("kr-intergalactic-transceiver", "kee-intergalactic-transceiver-loading")
+krastorio.technologies.removeUnlockRecipe("kr-intergalactic-transceiver", "kr-intergalactic-transceiver")
