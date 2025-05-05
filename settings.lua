33f09a3796a5cf89252cfdf9b1509039756f3d60
Michael Mönch <michael.moench@offerista.com>
2025-05-05 22:06:45 +0200

    updated mod for krastorio 2.0

--- settings.lua
+++ settings.lua
@@ -1,47 +1,42 @@
 data:extend({
-	{
-		type = "int-setting",
-		name = "kee-activations",
-		setting_type = "startup",
-		default_value = 10,
-		order = "a",
-        per_user = false,
+    {
+        type = "int-setting",
+        name = "kee-activations",
+        setting_type = "startup",
+        default_value = 10,
+        order = "a",
         allowed_values = {5,10,25}
     },
-	{
-		type = "int-setting",
-		name = "kee-initial-need-space-research",
-		setting_type = "startup",
-		default_value = 250,
-		order = "a",
-        per_user = false,
+    {
+        type = "int-setting",
+        name = "kee-initial-need-space-research",
+        setting_type = "startup",
+        default_value = 250,
+        order = "a",
         allowed_values = {100,250,500,1000,2000}
-	},
-	{
-		type = "int-setting",
-		name = "kee-initial-need-matter-research",
-		setting_type = "startup",
-		default_value = 1000,
-		order = "a",
-        per_user = false,
+    },
+    {
+        type = "int-setting",
+        name = "kee-initial-need-matter-research",
+        setting_type = "startup",
+        default_value = 1000,
+        order = "a",
         allowed_values = {100,250,500,1000,2000}
-	},
-	{
-		type = "int-setting",
-		name = "kee-initial-need-advanced-research",
-		setting_type = "startup",
-		default_value = 1000,
-		order = "a",
-        per_user = false,
+    },
+    {
+        type = "int-setting",
+        name = "kee-initial-need-advanced-research",
+        setting_type = "startup",
+        default_value = 1000,
+        order = "a",
         allowed_values = {100,250,500,1000,2000}
-	},
-	{
-		type = "int-setting",
-		name = "kee-initial-need-singularity-research",
-		setting_type = "startup",
-		default_value = 1000,
-		order = "a",
-        per_user = false,
+    },
+    {
+        type = "int-setting",
+        name = "kee-initial-need-singularity-research",
+        setting_type = "startup",
+        default_value = 1000,
+        order = "a",
         allowed_values = {100,250,500,1000,2000}
-	},
+    },
 })
